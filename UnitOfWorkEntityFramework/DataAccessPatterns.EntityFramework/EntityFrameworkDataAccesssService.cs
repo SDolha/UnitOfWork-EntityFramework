0f8b402c9f3887d070c278a300a0a2a78f83de3a
Sorin Dolha <SDolha@Hotmail.com>
2016-03-20 10:09:16 +0200

    Entity Framework data access service improvements.

--- UnitOfWorkEntityFramework/DataAccessPatterns.EntityFramework/EntityFrameworkDataAccesssService.cs
+++ UnitOfWorkEntityFramework/DataAccessPatterns.EntityFramework/EntityFrameworkDataAccesssService.cs
@@ -7,16 +7,14 @@ namespace DataAccessPatterns.EntityFramework
     // Provides data access support by passing Entity Framework implementations of IUnitOfWork and IRepository<T> bound to an instance of the specified database context type and its entity sets.
     // You can then always switch to a different data access technology by changing this mapping provided that you have IUnitOfWork and IRepository<T> implementations for the other technology ready, and the client side will not need to change.
     // Data object types (used by the database context) can be POCOs so Entity Framework could be entirely replaced with another system.
-    public class EntityFrameworkDataAccesssService<ContextType> : IDataAccesssService, IDisposable where ContextType : DbContext
+    public class EntityFrameworkDataAccesssService<TDbContext> : IDataAccesssService, IDisposable where TDbContext : DbContext, new()
     {
-        public EntityFrameworkDataAccesssService(ContextType dbContext)
+        public EntityFrameworkDataAccesssService()
         {
-            if (dbContext == null)
-                throw new ArgumentNullException("dbContext");
-            DbContext = dbContext;
+            DbContext = new TDbContext();
         }
 
-        protected ContextType DbContext { get; private set; }
+        protected TDbContext DbContext { get; private set; }
 
         // Initializes an Entity Framwork unit of work instance bound to the database context.
         public IUnitOfWork GetUnitOfWork()
--- UnitOfWorkEntityFramework/SampleApp.DataAccess/SampleDataAccesssService.cs
+++ UnitOfWorkEntityFramework/SampleApp.DataAccess/SampleDataAccesssService.cs
@@ -6,9 +6,6 @@ namespace SampleApp.DataAccess
     // Provides data access support to the client side by forwarding Entity Framework support to get IUnitOfWork and IRepository<T> of the generic Entity Framework data service implementation bound to the sample database context and its entity sets.
     public class SampleDataAccesssService : EntityFrameworkDataAccesssService<SampleDatabaseEntities>, ISampleDataAccesssService, IDisposable
     {
-        public SampleDataAccesssService() : base(new SampleDatabaseEntities())
-        { }
-
         // Adds support to initialize a specialized employee repository defined in the sample data access layer.
         public IEmployeeRepository GetEmployeeRepository()
         {
